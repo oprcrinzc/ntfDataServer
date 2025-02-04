73d6b948b9696e300d0d2b4690a9ae479a635239
oprc <chisainux@gmail.com>
2025-02-04 22:36:57 +0700

    prepare 1/10

--- db/db.go
+++ db/db.go
@@ -1,7 +1,21 @@
 package db
 
-import "go.mongodb.org/mongo-driver/v2/mongo"
+import (
+	"os"
 
-func conn() mongo.Collection {
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
 
+func conn() *mongo.Client {
+	uri := os.Getenv("DB_STRING")
+
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	return client
 }
+
+func GetCampaign()  {}
+func SaveCampaign() {}
